8e89f6083b786388bf8baa9a0a6b2f2e050d04b4
ranbarry <114967826+ranbarry@users.noreply.github.com>
2022-11-16 17:17:58 -0500

    Update play.js

--- play.js
+++ play.js
@@ -5,10 +5,9 @@ function exit(){
     
     }
 function begin() {
-    var x = prompt("How many tries would you like to guess the number from 1-100?")
+    var x = prompt("How many tries would you like to guess the number from 1-100?");
     var y = parseInt((Math.random()*(100-1+1))+1);
    
-        document.getElementById("this").innerHTML = y;
 
     
     for (i = x; i != 0; i = i - 1) {
