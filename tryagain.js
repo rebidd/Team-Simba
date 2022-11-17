121a31e15a656b8062e2ca669970fe0e18a688ea
ranbarry <114967826+ranbarry@users.noreply.github.com>
2022-11-16 19:06:57 -0500

    Update tryagain.js

--- tryagain.js
+++ tryagain.js
@@ -5,5 +5,27 @@ function exit(){
 
 
 function tryAgain(){
+    var x = parseInt(prompt("How many tries would you like to guess the number from 1-100?"));
+    var y = Math.floor((Math.random()*(100-1+1))+1);
+
+  
+    for (i = x; i > 0; i = i - 1) {
+        var z= parseInt(prompt("you have "+ i + " tries "));
+
+        if (z==y){
+            window.location.href = "congrats.html";
+        }
+  
+    }
+
+    if (z==y) {
+
+        window.location.href = "congrats.html";
+        
+    }
+    else{
+        window.location.href = "tryagain.html";
+
+    }
 
 }
