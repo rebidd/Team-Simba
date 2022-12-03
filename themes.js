bb30712b2f9abf678c8d5393d6c49cae9138564d
Kurt-Porley <92216256+Kurt-Porley@users.noreply.github.com>
2022-12-03 16:08:34 -0500

    Update themes.js

--- themes.js
+++ themes.js
@@ -1,3 +1,18 @@
+function theme1 (){
+    document.body.style.backgroundImage = "url('https://i.pinimg.com/originals/f3/8c/6e/f38c6e43c93de24b2beb536ad3d03dfe.gif')";
+}
+function theme2 (){
+    document.body.style.backgroundImage = "url('https://urbantoronto.ca/forum/attachments/ezgif-com-optimize-gif.163558/')";
+}
+
+function theme3 (){
+    document.body.style.backgroundImage = "url('https://i.pinimg.com/originals/56/e9/36/56e9367fa1d4ec7a49a23cbe9f979c0c.gif')";
+}
+
+function theme4 (){
+    document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/4279556.jpg')";
+}
+
 function exit(){
 
     window.location.href = "simba.html"
