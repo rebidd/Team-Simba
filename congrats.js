f9b4cedc84ff1e3a4be8896c90777f87d2b47d9b
ranbarry <114967826+ranbarry@users.noreply.github.com>
2022-12-02 19:01:32 -0500

    Update congrats.js

--- congrats.js
+++ congrats.js
@@ -1,11 +1,18 @@
 function exit() {
     window.location.href = "simba.html";
 }
+function leaderBoard() {
+    window.location.href = "leaderboard.html";
+    
+}
+function playAgain() {
+    window.location.href = "play.html";
+}
 window.onload = function(){
     var op=document.getElementById("op");
     var myName;
     myName = prompt("You Win!! Please enter your name", "");
+   
     
-    
-    op.innerHTML = myName;
+    op.innerHTML ="Challenger's name"+"<br>" +myName;
 }
