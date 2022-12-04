c15a369b2a8adfa84c227d44706f169d79996abd
Simba <114967466+rebidd@users.noreply.github.com>
2022-12-04 17:11:10 -0500

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -1,43 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Leaderboard</title>
+function exit(){
 
-    <link rel="stylesheet" href="simba.css">
+    window.location.href = "simba.html"
     
-
-    <script src="leaderboard.js"></script>
-  
-</head>
-<body>
-
-    <h1> Leaderboard </h1>
-
-    <div id="row1">
-        <div id="name">1. <span id = "new"></span></div><div id="attempts"></div><div id="guesses"></div>
-    </div>
-    <div id="row2">
-        <div id="name">2. <span id="guy"></span></div><div id="attempts"></div><div id="guesses"></div>
-    </div>
-    <div id="row3">
-        <div id="name">3. <span id="guy"></span></div><div id="attempts"></div><div id="guesses"></div>
-    </div>
-    <div id="row">
-        <div id="name">4. <span id="guy"></span></div><div id="attempts"></div><div id="guesses"></div>
-    </div>
-    <div id="row">
-        <div id="name">5. <span id="guy"></span></div><div id="attempts"></div><div id="guesses"></div>
-    </div>
-    <div id="row">
-        <div id="name">6. <span id="guy"></span></div><div id="attempts"></div><div id="guesses"></div>
-    </div>
-
-      <button id = "exit" onclick="exit()">Exit</button>
-
-  
-</body>
-</html>
-    
+    }
